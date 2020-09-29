f7c0253cbe46d6c0e45b5d52d0dbf5911e5ddf0f
sangryul <ksl970330@naver.com>
2020-09-28 23:42:20 -0700

    add shell script about multiplication

---
+++ assignment3/multi-table.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ ! $1 || ! $1 =~ ^[-+]?[0-9]+$ ]];
+then
+	echo "You have No first argument or wrong number on first argument"
+	exit 1
+elif [[ ! $1 || ! $1 =~ ^[-+]?[0-9]+$ ]];
+then
+	echo "You have No second argument or wrong number on second argument"
+	exit 1
+fi
+
+for i in $(seq 1 $1)
+do
+	for j in $(seq 1 $2)
+	do
+		mul=`expr $i \* $j`
+		echo -n "$i*$j=$mul "
+	done
+	echo ""
+done
+
+exit 0
